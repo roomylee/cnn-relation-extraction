3e4e0989bad7733d65fc36d2a09212bfe8f1b0ee
roomylee <roomylee@naver.com>
2018-10-25 21:34:09 +0900

    fix evaluation to compute official score

--- eval.py
+++ eval.py
@@ -1,55 +1,27 @@
 import tensorflow as tf
 import numpy as np
 import os
+import subprocess
+
 import data_helpers
-from sklearn.metrics import f1_score
-import warnings
-import sklearn.exceptions
-warnings.filterwarnings("ignore", category=sklearn.exceptions.UndefinedMetricWarning)
-
-
-# Parameters
-# ==================================================
-
-# Data loading params
-tf.flags.DEFINE_string("eval_dir", "SemEval2010_task8_all_data/SemEval2010_task8_testing_keys/TEST_FILE_FULL.TXT", "Path of evaluation data")
-tf.flags.DEFINE_string("output_dir", "result/prediction.txt", "Path of prediction for evaluation data")
-tf.flags.DEFINE_string("target_dir", "result/answer.txt", "Path of target(answer) file for evaluation data")
-
-# Eval Parameters
-tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (Default: 64)")
-tf.flags.DEFINE_string("checkpoint_dir", "", "Checkpoint directory from training run")
-
-# Misc Parameters
-tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
-tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
-
-
-FLAGS = tf.flags.FLAGS
-FLAGS._parse_flags()
-print("\nParameters:")
-for attr, value in sorted(FLAGS.__flags.items()):
-    print("{} = {}".format(attr.upper(), value))
-print("")
+import utils
+from configure import FLAGS
 
 
 def eval():
     with tf.device('/cpu:0'):
-        x_text, pos1, pos2, y = data_helpers.load_data_and_labels(FLAGS.eval_dir)
+        x_text, y, pos1, pos2 = data_helpers.load_data_and_labels(FLAGS.test_path)
 
     # Map data into vocabulary
     text_path = os.path.join(FLAGS.checkpoint_dir, "..", "text_vocab")
     text_vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor.restore(text_path)
-    text_vec = np.array(list(text_vocab_processor.transform(x_text)))
+    x = np.array(list(text_vocab_processor.transform(x_text)))
 
     # Map data into position
-    position_path = os.path.join(FLAGS.checkpoint_dir, "..", "position_vocab")
+    position_path = os.path.join(FLAGS.checkpoint_dir, "..", "pos_vocab")
     position_vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor.restore(position_path)
-    pos1_vec = np.array(list(position_vocab_processor.transform(pos1)))
-    pos2_vec = np.array(list(position_vocab_processor.transform(pos2)))
-
-    x_eval = np.array([list(i) for i in zip(text_vec, pos1_vec, pos2_vec)])
-    y_eval = np.argmax(y, axis=1)
+    p1 = np.array(list(position_vocab_processor.transform(pos1)))
+    p2 = np.array(list(position_vocab_processor.transform(pos2)))
 
     checkpoint_file = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
 
@@ -66,8 +38,8 @@ def eval():
 
             # Get the placeholders from the graph by name
             input_text = graph.get_operation_by_name("input_text").outputs[0]
-            input_pos1 = graph.get_operation_by_name("input_pos1").outputs[0]
-            input_pos2 = graph.get_operation_by_name("input_pos2").outputs[0]
+            input_p1 = graph.get_operation_by_name("input_p1").outputs[0]
+            input_p2 = graph.get_operation_by_name("input_p2").outputs[0]
             # input_y = graph.get_operation_by_name("input_y").outputs[0]
             dropout_keep_prob = graph.get_operation_by_name("dropout_keep_prob").outputs[0]
 
@@ -75,41 +47,37 @@ def eval():
             predictions = graph.get_operation_by_name("output/predictions").outputs[0]
 
             # Generate batches for one epoch
-            batches = data_helpers.batch_iter(list(x_eval), FLAGS.batch_size, 1, shuffle=False)
+            batches = data_helpers.batch_iter(list(zip(x, p1, p2)), FLAGS.batch_size, 1, shuffle=False)
 
             # Collect the predictions here
-            all_predictions = []
-            for x_eval_batch in batches:
-                x_batch = np.array(x_eval_batch).transpose((1, 0, 2))
-                batch_predictions = sess.run(predictions, {input_text: x_batch[0],
-                                                           input_pos1: x_batch[1],
-                                                           input_pos2: x_batch[2],
-                                                           dropout_keep_prob: 1.0})
-                all_predictions = np.concatenate([all_predictions, batch_predictions])
+            preds = []
+            for batch in batches:
+                x_batch, p1_batch, p2_batch = zip(*batch)
+                pred = sess.run(predictions, {input_text: x_batch,
+                                              input_p1: p1_batch,
+                                              input_p2: p2_batch,
+                                              dropout_keep_prob: 1.0})
+                preds.append(pred)
+            preds = np.concatenate(preds)
+            truths = np.argmax(y, axis=1)
 
-            labelsMapping = {0: 'Other',
-                             1: 'Message-Topic(e1,e2)', 2: 'Message-Topic(e2,e1)',
-                             3: 'Product-Producer(e1,e2)', 4: 'Product-Producer(e2,e1)',
-                             5: 'Instrument-Agency(e1,e2)', 6: 'Instrument-Agency(e2,e1)',
-                             7: 'Entity-Destination(e1,e2)', 8: 'Entity-Destination(e2,e1)',
-                             9: 'Cause-Effect(e1,e2)', 10: 'Cause-Effect(e2,e1)',
-                             11: 'Component-Whole(e1,e2)', 12: 'Component-Whole(e2,e1)',
-                             13: 'Entity-Origin(e1,e2)', 14: 'Entity-Origin(e2,e1)',
-                             15: 'Member-Collection(e1,e2)', 16: 'Member-Collection(e2,e1)',
-                             17: 'Content-Container(e1,e2)', 18: 'Content-Container(e2,e1)'}
-            output_file = open(FLAGS.output_dir, 'w')
-            target_file = open(FLAGS.target_dir, 'w')
-            for i in range(len(all_predictions)):
-                output_file.write("{}\t{}\n".format(i, labelsMapping[all_predictions[i]]))
-                target_file.write("{}\t{}\n".format(i, labelsMapping[y_eval[i]]))
-            output_file.close()
-            target_file.close()
+            prediction_path = os.path.join(FLAGS.checkpoint_dir, "..", "predictions.txt")
+            truth_path = os.path.join(FLAGS.checkpoint_dir, "..", "ground_truths.txt")
+            prediction_file = open(prediction_path, 'w')
+            truth_file = open(truth_path, 'w')
+            for i in range(len(preds)):
+                prediction_file.write("{}\t{}\n".format(i, utils.label2class[preds[i]]))
+                truth_file.write("{}\t{}\n".format(i, utils.label2class[truths[i]]))
+            prediction_file.close()
+            truth_file.close()
 
-            correct_predictions = float(sum(all_predictions == y_eval))
-            print("\nTotal number of test examples: {}".format(len(y_eval)))
-            print("Accuracy: {:g}".format(correct_predictions / float(len(y_eval))))
-            print("(2*9+1)-Way Macro-Average F1 Score (excluding Other): {:g}".format(
-                f1_score(y_eval, all_predictions, labels=np.array(range(1, 19)), average="macro")))
+            perl_path = os.path.join(os.path.curdir,
+                                     "SemEval2010_task8_all_data",
+                                     "SemEval2010_task8_scorer-v1.2",
+                                     "semeval2010_task8_scorer-v1.2.pl")
+            process = subprocess.Popen(["perl", perl_path, prediction_path, truth_path], stdout=subprocess.PIPE)
+            for line in str(process.communicate()[0].decode("utf-8")).split("\\n"):
+                print(line)
 
 
 def main(_):
